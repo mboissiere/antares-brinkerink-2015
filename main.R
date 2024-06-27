9c2068ccccd32dde47373ac28bd37d98a65449f6
Mattéo Boissière <135631616+mboissiere@users.noreply.github.com>
2024-06-27 14:43:30 +0200

    First successful parse of load data, revealed incoherence in nodes CSV

--- main.R
+++ main.R
@@ -75,9 +75,9 @@ for (row in 1:nrow(deane_nodes_df)) {
       localization = c(x, y)
     )
   }, error = function(e) {
-    cat("Error in get_country_coordinates(", country_code, ") :\n")
-    cat("  ", conditionMessage(e), "\n")
-    cat("Skipping this country code and continuing...\n\n")
+    #cat("Error in get_country_coordinates(", country_code, ") :\n")
+    #cat("  ", conditionMessage(e), "\n")
+    cat("Error : skipping creation of node ", zone, " and continuing...\n")
   })
   
   # Continue parsing data or performing other operations
@@ -110,16 +110,33 @@ load_data_matrix <- read.table(
 for (zone in colnames(load_data_matrix)) {
   # Extract the time series for the current column
   load_ts <- load_data_matrix[[zone]]
-  
-  writeInputTS(
-    data = load_ts,
-    type = "load",
-    area = zone
-  )
   cat(paste("Adding", zone, "load data...\n"))
+  tryCatch({
+    writeInputTS(
+      data = load_ts,
+      type = "load",
+      area = zone
+    )
+  }, error = function(e) {
+    cat("Error : node ", zone, "not found, skipping...\n")
+  }
+  )
 }
 
 cat("Done adding load data!\n")
+
+#Adding AS-SGP load data...
+#Erreur : 'NA-CAN' is not a valid area name, possible names are: af-ago, af-bdi, af-ben, af-bfa, af-bwa, af-caf, af-civ, af-cmr, af-cod, af-cog, af-cpv, af-dji, af-dza, af-egy, af-eri, af-esh, af-eth, af-gab, af-gha, af-gin, af-gmb, af-gnb, af-gnq, af-ken, af-lbr, af-lby, af-lso, af-mar, af-mdg, af-mli, af-moz, af-mrt, af-mus, af-mwi, af-nam, af-ner, af-nga, af-rwa, af-sdn, af-sen, af-sle, af-swz, af-tgo, af-tun, af-tza, af-uga, af-zaf, af-zmb, af-zwe, as-afg, as-are, as-bgd, as-bhr, as-brn, as-btn, as-chn-an, as-chn-be, as-chn-ch, as-chn-em, as-chn-fu, as-chn-ga, as-chn-gd, as-chn-gu, as-chn-gx, as-chn-ha, as-chn-hb, as-chn-he, as-chn-hj, as-chn-hk, as-chn-hn, as-chn-hu, as-chn-ji, as-chn-js, as-chn-jx, as-chn-li, as-chn-ma, as-chn-ni, as-chn-qi, as-chn-sc, as-chn-sd, as-chn-sh, as-chn-si, as-chn-sx, as-chn-ti, as-chn-tj, as-chn-wm, as-chn-xi, as-chn-yu, as-chn-zh, as-idn, as-ind-ea, as-ind-ne, as-ind-no, as-ind-so, as-ind-we, as-irn, as-irq, as-isr, as-jor, as-jpn-ce, as-jpn-ho, as-j
+#> 
+#  > cat("Done adding load data!\n")
+
+# Exception moment (on s'en fout de NA-CAN c'est un agrégat)
+
+# NB : en faisant ce petit travail, on remarque qu'il y a des agrégats qui buggent oui,
+# mais pas que.
+# Dans les nodes qu'il faudra possiblement rajouter à la main :
+# AF-SOM, AF-TCD, AS-TLS, EU-MLT, NA-BLZ, NA-HTI, SA-SUR
+# (heureusement que j'ai pas fait un for zone in zones !!)
 
 ####################################################
 
@@ -142,12 +159,12 @@ for (row in 1:nrow(ntc_df)) {
   to_node = ntc_df$To[row]
   ntc_direct = ntc_df$Max.Flow..MW.[row]
   ntc_indirect = -ntc_df$Min.Flow..MW.[row]
-  #if (ntc_direct == 0 & ntc_indirect == 0){
+  if (ntc_direct == 0 & ntc_indirect == 0){
     # Peut etre jouer avec l'évaluation paresseuse ?
-    #cat(paste("Skipping ", from_node, " to ", to_node, " link (zero capacity)\n"))
+    cat(paste("Skipping ", from_node, " to ", to_node, " link (zero capacity)\n"))
     # nb : c'est bad long ces prints
-  #} else {
-    #ts_link <- data.frame(rep(ntc_direct, 8760), rep(ntc_indirect, 8760))
+  } else {
+    ts_link <- data.frame(rep(ntc_direct, 8760), rep(ntc_indirect, 8760))
     tryCatch({
       # Function that may throw an error
       createLink(
@@ -167,7 +184,7 @@ for (row in 1:nrow(ntc_df)) {
     )
     
   # TODO : faire un paramètre "include Deane null links" TRUE ou FALSE
-  #}
+  }
 }
 
 cat("Done adding links !")
