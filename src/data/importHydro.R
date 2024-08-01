70001a6d1ace8c6dcff0599f997c0b31762a1604
Mattéo Boissière <135631616+mboissiere@users.noreply.github.com>
2024-08-01 17:35:34 +0200

    First steps towards hydro implementation, will continue tomorrow

--- src/data/importHydro.R
+++ src/data/importHydro.R
@@ -10,37 +10,38 @@ library(tidyr)
 generators_tbl <- full_2015_generators_tbl %>%
   filter(fuel_type == "Hydro")
 
-# print(generators_tbl)
+getHydroGeneratorsProperties <- function() {
+  hydro_generators_tbl <- full_2015_generators_tbl %>%
+    filter(fuel_type == "Hydro") %>%
+    select(generator_name, node)
+  
+  hydro_properties_tbl <- getTableFromPlexos(PROPERTIES_PATH) %>%
+    filter(collection == "Generators") %>%
+    mutate(generator_name = toupper(child_object)) %>%
+    select(generator_name, property, value)
+  
+  hydro_generators_tbl <- hydro_generators_tbl %>%
+    left_join(hydro_properties_tbl, by = "generator_name") %>%
+    pivot_wider(names_from = property, values_from = value) %>%
+    mutate(nominal_capacity = `Max Capacity` * Units) %>%
+    select(generator_name, node, nominal_capacity, `Max Capacity`, Units)
+  
+  hydro_generators_tbl <- hydro_generators_tbl %>%
+    select(generator_name, node, nominal_capacity)
+  
+  return(hydro_generators_tbl)
+}
 
-# # Avant de faire un tableau pour l'import dans Antares, répondre à la demande
-# # de Nicolas, qui est de produire un tableau (objet R ? CSV depuis objet R ?)
-# # avec la somme des productions par technologie (et la méthodologie)
-# 
-# getHydroPropertiesTable <- function(generators_tbl) {
-#   hydro_generators_tbl <- generators_tbl %>%
-#     filter(fuel_type == "Hydro")
-# 
-#   properties_tbl <- getTableFromPlexos(PROPERTIES_PATH) %>%
-#     filter(property %in% c("Max Capacity", "Units")) %>%
-#     rename(generator_name = child_object) %>%
-#     mutate(generator_name = toupper(generator_name)) %>%
-#     select(generator_name, property, value)
-# 
-#   wind_properties_tbl <- wind_generators_tbl %>%
-#     left_join(properties_tbl, by = "generator_name") %>%
-#     pivot_wider(names_from = property, values_from = value)
-# 
-# 
-#   wind_properties_tbl <- wind_properties_tbl %>%
-#     rename(
-#       nominal_capacity = "Max Capacity",
-#       units = Units
-#     ) %>%
-#     select(generator_name, node, cluster_type, nominal_capacity, units)
-# 
-#   return(wind_properties_tbl)
-# }
-# 
+hydro_generators_tbl <- getHydroGeneratorsProperties()
+print(hydro_generators_tbl)
+
+# Ah mais en vrai on va effectivement sommer par pays. 
+# Et puis lire les monthly de Ninja en en faisant de l'horaire.
+# Il faudrait d'ailleurs demander aux auteurs de Deane : dans les simus qui
+# ont donné leurs graphiques, ont-ils utilisé 2015 ou 15 year average pour l'hydro.
+
+
+
 # # Nota bene : vu comment marchent les Generators Hydro pour lesquels il y a
 # # une timeseries mensuelle, je pense qu'on peut juste faire bourrinnement pour chaque centrale
 # # décharge = max capacity x units x facteur de charge, sans séparer par centrale (pilotabilité des 5 units etc)
@@ -50,21 +51,6 @@ generators_tbl <- full_2015_generators_tbl %>%
 
 
 getTotalHydroGeneratorsCapacityPerCountry <- function() {
-  hydro_generators_tbl <- full_2015_generators_tbl %>%
-    filter(fuel_type == "Hydro") %>%
-    select(generator_name, node)
-  
-  print(hydro_generators_tbl)
-  
-  hydro_properties_tbl <- getTableFromPlexos(PROPERTIES_PATH) %>%
-    #filter(collection == "Batteries")
-    filter(collection == "Generators") %>%
-    mutate(generator_name = toupper(child_object)) %>%
-    #rename(generator_name = child_object) %>%
-    select(generator_name, property, value)
-  # Et encore une fois, je me fais avoir par la capitalisation ! Bon Dieu !
-  
-  print(hydro_properties_tbl)
   
   hydro_generators_tbl <- hydro_generators_tbl %>%
     left_join(hydro_properties_tbl, by = "generator_name") %>%
