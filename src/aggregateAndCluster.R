c8340a16e4180286b16155f9b22a92107b769fb9
Mattéo Boissière <135631616+mboissiere@users.noreply.github.com>
2024-08-19 14:31:07 +0200

    Finished implementation of clustering for batteries, but also stashed it for now.

--- src/aggregateAndCluster.R
+++ src/aggregateAndCluster.R
@@ -7,7 +7,7 @@ source(".\\src\\utils.R")
 source("parameters.R")
 
 
-batteries_tbl <- readRDS(".\\src\\objects\\full_2015_batteries_tbl.rds")
+# batteries_tbl <- readRDS(".\\src\\objects\\full_2015_batteries_tbl.rds")
 # print(batteries_tbl)
 # print(batteries_tbl, n = 200)
 
@@ -92,16 +92,20 @@ aggregateEquivalentBatteries <- function(batteries_tbl) {
   # print(aggregated_batteries_tbl, n = 50)
   
   aggregated_batteries_tbl <- aggregated_batteries_tbl %>%
-    group_by(node, cluster_type, continent, battery_group, max_power, efficiency) %>% # efficiency should be redundant with cluster type
+    group_by(node, cluster_type, continent, battery_group, max_power, efficiency, capacity) %>% 
+    # efficiency should be redundant with cluster type
     # and so is continent, battery group... technically this function isn't very robust because it's not even sure these columns exist
     # (again, i lose some levels of abstraction by preprocessing data then saving it to objects then reading it...)
     
     # aah !! i summed units for batteries that aren't functionally the same !! this is really bad i can't believe i almost let this slide
     # but what can i do... a mean of capacity perhaps ???
     # this is really weird if there's like 1 huge dam and 1 small dam...
+    
+    # going to keep this logic of only aggregating FUNCTIONALLY SIMILAR BATTERIES
+    # in a way that is ONLY A PREPROCESSING OF K-MEANS CLUSTERING so it doesnt mess up later
+    # this MEANS : GROUPING BY MAX POWER AND CAPACITY.
   summarize(
     total_units = sum(units), # lmao it's units here and nb_units there i s2g when will The Great Standardization come
-    total_capacity = sum(capacity),
     combined_names = paste0(
       unique(getPrefix(battery_name))[1],  
       paste(
@@ -114,7 +118,7 @@ aggregateEquivalentBatteries <- function(batteries_tbl) {
   aggregated_batteries_tbl <- aggregated_batteries_tbl %>%
     mutate(battery_name = truncateStringVec(combined_names, CLUSTER_NAME_LIMIT),
            units = total_units,
-           capacity =  total_capacity,
+           # capacity =  total_capacity,
            initial_state = 50, # it's always 50 # dunno if we should keep it as a percentage or not in tibble but eh
     ) %>%
     select(battery_name, continent, node, battery_group, cluster_type, units, capacity, max_power, efficiency, initial_state)
@@ -122,15 +126,52 @@ aggregateEquivalentBatteries <- function(batteries_tbl) {
   return(aggregated_batteries_tbl)
 }
 
-batteries_tbl <- readRDS(".\\src\\objects\\full_2015_batteries_tbl.rds")
-print(batteries_tbl, n = 50)
+# batteries_tbl <- readRDS(".\\src\\objects\\full_2015_batteries_tbl.rds")
+# print(batteries_tbl, n = 50)
+# 
+# aggregated_batteries_tbl <- aggregateEquivalentBatteries(batteries_tbl)
+# saveRDS(aggregated_batteries_tbl, ".\\src\\objects\\batteries_aggregated_tbl.rds")
+# aggregated_batteries_tbl <- readRDS(".\\src\\objects\\batteries_aggregated_tbl.rds")
+# print(aggregated_batteries_tbl, n  = 50)
+# print(aggregated_batteries_tbl %>% select(-battery_group, -initial_state, -continent), n  = 50)
 
-aggregated_batteries_tbl <- aggregateEquivalentBatteries(batteries_tbl)
-saveRDS(aggregated_batteries_tbl, ".\\src\\objects\\batteries_aggregated_tbl.rds")
-aggregated_batteries_tbl <- readRDS(".\\src\\objects\\batteries_aggregated_tbl.rds")
-print(aggregated_batteries_tbl, n  = 50)
+# Aggregation of batteries turns 1108 rows into 891 (THE OLD ONE THAT WAS BAD)
+# Aggregation of batteries turns 1108 rows into 902 (the new one that seems less bad)
 
-# Aggregation of batteries turns 1108 rows into 891
+# # let's check that it worked well :
+# print(batteries_tbl %>% filter(node == "AF-ZAF"), n = 50)
+# print(aggregated_batteries_tbl %>% filter(node == "AF-ZAF"), n = 50)
+# 
+# # # A tibble: 8 x 10
+# # battery_name                continent node   battery_group cluster_type units capacity max_power initial_state efficiency
+# # <chr>                       <chr>     <chr>  <chr>         <chr>        <dbl>    <dbl>     <dbl>         <dbl>      <dbl>
+# #   1 ZAF_CHE_RustenburgVRLAP1244 Africa    AF-ZAF Chemical Bat~ Battery          1     0.02      0.01            50         80
+# # 2 ZAF_PHS_DrakensbergPump1245 Africa    AF-ZAF Pumped Hydro~ PSP_closed       5 18902.      200               50         75
+# # 3 ZAF_PHS_PalmietPumpedSt1246 Africa    AF-ZAF Pumped Hydro~ PSP_closed       2  7561.      200               50         75
+# # 4 ZAF_PHS_SteenbrasDamPum1247 Africa    AF-ZAF Pumped Hydro~ PSP_closed       1  3402.      180               50         75
+# # 5 ZAF_THE_BokpoortConcent1248 Africa    AF-ZAF Thermal       Other1           1   440        55               50         50
+# # 6 ZAF_THE_KaxuSolarOne1249    Africa    AF-ZAF Thermal       Other1           1   800       100               50         50
+# # 7 ZAF_THE_KhiSolarOnePowe1250 Africa    AF-ZAF Thermal       Other1           1   400        50               50         50
+# # 8 ZAF_THE_XinaSolarOnePow1251 Africa    AF-ZAF Thermal       Other1           1   800       100               50         50
+# # > print(aggregated_batteries_tbl %>% filter(node == "AF-ZAF"), n = 50)
+# # # A tibble: 7 x 10
+# # battery_name                 continent node  battery_group cluster_type units capacity max_power efficiency initial_state
+# # <chr>                        <chr>     <chr> <chr>         <chr>        <dbl>    <dbl>     <dbl>      <dbl>         <dbl>
+# #   1 ZAF_CHE_RustenburgVRLAP1244  Africa    AF-Z~ Chemical Bat~ Battery          1     0.02      0.01         80            50
+# # 2 ZAF_THE_KhiSolarOnePowe1250  Africa    AF-Z~ Thermal       Other1           1   400        50            50            50
+# # 3 ZAF_THE_BokpoortConcent1248  Africa    AF-Z~ Thermal       Other1           1   440        55            50            50
+# # 4 ZAF_THE_KaxuSolarOne1249_Xi~ Africa    AF-Z~ Thermal       Other1           2   800       100            50            50
+# # 5 ZAF_PHS_SteenbrasDamPum1247  Africa    AF-Z~ Pumped Hydro~ PSP_closed       1  3402.      180            75            50
+# # 6 ZAF_PHS_PalmietPumpedSt1246  Africa    AF-Z~ Pumped Hydro~ PSP_closed       1 15122.      400            75            50
+# # 7 ZAF_PHS_DrakensbergPump1245  Africa    AF-Z~ Pumped Hydro~ PSP_closed       1 94510.     1000            75            50
+# # 
+# # Indeed, KaxuSolarOne and XinaSolarOne are truly similar.
+# 
+# 46 CHN_CHE_TheZhangbeiProj170                                        AS-C~ Battery          1  8   e+0     2             75
+# 47 CHN_CHE_ZhangbeiNationa180                                        AS-C~ Battery          1  2.4 e+1     2             75
+# 
+# And those have the same max_power, but not the same capacity, so they are seperated.
+
 
 ######################
 
@@ -225,42 +266,34 @@ clusteringForGenerators <- function(aggregated_generators_tbl, max_clusters) {
 cluster_and_summarize_batteries <- function(df, k, node, cluster_type, efficiency) { # continent, battery_group)
   # on met en argument tout ce qu'on regarde pas du coup ? tout ce qui st pareil ?
   # faisons avec le moins de redondance pour l'instant.
-  print("df:")
-  print(df)
-  # Check if the number of rows is greater than k
+  print(paste("df:", node))
+  print(df, n = 25)
   if (nrow(df) > k) {
-    # Perform k-means clustering on the aggregated data's nominal_capacity
-    clusters <- kmeans(df$max_power, centers = k)
+    clusters <- kmeans(df[, c("max_power", "capacity")], centers = k) # 2-dimensional clustering here !
     df$cluster <- as.factor(clusters$cluster)
   } else {
-    # If there are fewer rows than k, each row becomes its own cluster
     df$cluster <- as.factor(1:nrow(df))
   }
-  print("df with clusters")
-  print(df)
+  print(paste("df with clusters:", node))
+  print(df, n = 25)
   
-  # Summarize the clusters
   summary <- df %>%
     group_by(cluster) %>%
     summarise(
       combined_names = paste0(
-        unique(getPrefix(battery_name))[1],  # Extract and keep the prefix only once
+        unique(getPrefix(battery_name))[1],
         paste(
-          unique(sapply(battery_name, removePrefix)),  # Remove the prefix and combine unique names
+          unique(sapply(battery_name, removePrefix)),
           collapse = "_"
         )
       ),
-      # max_power = mean(max_power),
-      # Testons sans ?
-      # pareil sans efficiency
-      units = sum(units),
-      capacity = sum(capacity),
+      max_power = mean(max_power), 
+      capacity = mean(capacity), 
+      nb_units = sum(units),
       initial_state = 50,
       .groups = 'drop'
     )
   print(summary)
-  # prints are super interesting to keep track of clustering.
-  # perhaps create a seperate clusteringLog ?
   
   summary <- summary %>%
     mutate(
@@ -292,25 +325,31 @@ clusteringForBatteries <- function(batteries_aggregated_tbl, max_clusters) {
     group_by(node, cluster_type, efficiency) %>%
     nest() %>%
     mutate(
-      clustered_data = map(data, ~ cluster_and_summarize_generators(.x, k = max_clusters, node, cluster_type, efficiency))
+      clustered_data = map(data, ~ cluster_and_summarize_batteries(.x, k = max_clusters, node, cluster_type, efficiency))
     ) %>%
     unnest(clustered_data) %>%
-    select(battery_name, node, cluster_type, units, capacity, max_power, efficiency, initial_state)
+    select(battery_name, node, cluster_type, nb_units, capacity, max_power, efficiency, initial_state)
   return(batteries_clusters_tbl)
 }
 
-# Test on batteries
+# # Test on batteries
+# 
+# batteries_tbl <- readRDS(".\\src\\objects\\full_2015_batteries_tbl.rds")
+# 
+# batteries_aggregated_tbl <- readRDS(".\\src\\objects\\batteries_aggregated_tbl.rds")
+# 
+# max_clusters = 5
+# batteries_clusters_tbl <- clusteringForBatteries(batteries_aggregated_tbl, max_clusters)
+# 
+# print("Batteries full table:")
+# print(batteries_tbl, n = 50)
+# 
+# print("Batteries aggregated table:")
+# print(batteries_aggregated_tbl, n = 50)
+# 
+# print(paste0(max_clusters,"-clustering for dataset:"))
+# print(batteries_clusters_tbl, n = 50)
+# 
+# ###
+# # Determining the best method of clustering for batteries is complicated for now. We can do this later.
 
-batteries_tbl <- readRDS(".\\src\\objects\\full_2015_batteries_tbl.rds")
-print("Batteries full table:")
-print(batteries_tbl, n = 50)
-
-batteries_aggregated_tbl <- readRDS(".\\src\\objects\\batteries_aggregated_tbl.rds")
-print("Batteries aggregated table:")
-print(batteries_aggregated_tbl, n = 50)
-
-max_clusters = 10
-batteries_clusters_tbl <- clusteringForGenerators(batteries_aggregated_tbl, max_clusters)
-print(paste0(max_clusters,"-clustering for dataset:"))
-print(batteries_clusters_tbl, n = 50)
-# saveRDS(object = thermal_clusters_tbl, file = ".\\src\\objects\\thermal_5clustering_tbl.rds")
