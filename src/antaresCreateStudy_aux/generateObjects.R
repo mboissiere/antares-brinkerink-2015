a015a0893558361b0e8caf452fd2904386d65466
Mattéo Boissière <135631616+mboissiere@users.noreply.github.com>
2024-09-09 14:22:59 +0200

    Made a base generators table, which includes more data but also commission date, which helps check if generators were active in 2015, but could be generalized to other years as well!

--- src/antaresCreateStudy_aux/generateObjects.R
+++ src/antaresCreateStudy_aux/generateObjects.R
@@ -5,14 +5,21 @@ deane_all_nodes_path <- file.path(OBJECTS_PATH, deane_all_nodes_name)
 if (REGENERATE_OBJECTS | !file.exists(deane_all_nodes_path)) {
   source(addNodes_module)
   deane_all_nodes_lst <- getAllNodes()
+  # So far, AllNodes isn't tolowered.
   saveRDS(object = deane_all_nodes_lst,
           file = deane_all_nodes_path)
 }
 
 
-base_generator_name = "base_generators_tbl.rds"
-if (REGENERATE_OBJECTS | !file.exists(file.path(OBJECTS_PATH, base_generator_name))) {
+base_generators_name = "base_generators_tbl.rds"
+base_generators_path <- file.path(OBJECTS_PATH, base_generators_name)
+if (REGENERATE_OBJECTS | !file.exists(base_generator_path)) {
   source(preprocessPlexosData_module)
   base_generators_tbl <- getGeneratorsFromNodes(deane_all_nodes_lst)
-  print(base_generators_tbl)
+  base_generators_tbl <- addGeneralFuelInfo(base_generators_tbl)
+  base_generators_tbl <- getBaseGeneratorData(base_generators_tbl)
+  print(base_generators_tbl, n = 200)
+  # print(base_generators_tbl %>% filter(node == "as-jpn-ce" & antares_cluster_type == "Nuclear"))
+  saveRDS(object = base_generators_tbl,
+          file = base_generators_path)
 }
--- src/antaresCreateStudy_aux/preprocessPlexosData.R
+++ src/antaresCreateStudy_aux/preprocessPlexosData.R
@@ -5,7 +5,7 @@ library(roxygen2)
 
 PLEXOS_PATH = file.path("input", "dataverse_files", "PLEXOS-World 2015 Gold V1.1")
 
-OBJECTS_PATH = file.path(PLEXOS_PATH, "Objects.txt")
+PLEXOS_OBJECTS_PATH = file.path(PLEXOS_PATH, "Objects.txt")
 MEMBERSHIPS_PATH = file.path(PLEXOS_PATH, "Memberships.txt")
 ATTRIBUTES_PATH = file.path(PLEXOS_PATH, "Attributes.txt")
 PROPERTIES_PATH = file.path(PLEXOS_PATH, "Properties.txt")
@@ -121,7 +121,7 @@ apply2015NuclearFilter <- function(generators_tbl) {
 
 getGeneratorsFromNodes <- function(nodes) {
   # Initialization of generators table with only generator names and continent of origin
-  generators_tbl <- getTableFromPlexos(OBJECTS_PATH) %>%
+  generators_tbl <- getTableFromPlexos(PLEXOS_OBJECTS_PATH) %>%
     filter(class == "Generator") %>%
     select(name, category) %>%
     rename(generator_name = name,
@@ -134,14 +134,17 @@ getGeneratorsFromNodes <- function(nodes) {
            node = child_object) %>%
     left_join(generators_tbl, by = "generator_name")
   
-  # Forcing capital letters on generator names to avoid discrepancies with Ninja dataset
-  generators_tbl <- generators_tbl %>%
-    mutate(generator_name = tolower(generator_name))
-  
   # Keep only nodes of interest
   generators_tbl <- generators_tbl %>%
     filter(node %in% nodes) %>%
     select(generator_name, continent, node)
+  
+  # Forcing capital letters on generator names to avoid discrepancies with Ninja dataset
+  generators_tbl <- generators_tbl %>%
+    mutate(generator_name = tolower(generator_name),
+           continent = tolower(continent),
+           node = tolower(node))
+  # and actually on more stuff to never worry about capitalization, antares uses small letters anyway
   
   return(generators_tbl)
 }
@@ -168,11 +171,11 @@ filterFor2015 <- function(generators_tbl) {
 # print(example)
 
 addGeneralFuelInfo <- function(generators_tbl) {
-  fuels_tbl <- getTableFromPlexos(OBJECTS_PATH) %>%
+  fuels_tbl <- getTableFromPlexos(PLEXOS_OBJECTS_PATH) %>%
     filter(class == "Fuel") %>%
     select(name, category) %>%
-    rename(fuel_group = name,
-           fuel_type = category)
+    rename(plexos_fuel_group = name,
+           plexos_fuel_type = category)
   
   # Une mission à faire je pense : refactorer le code pour qu'il fasse genre.
   # Je prends les trucs. Je fous les wind dans un objet R. Et j'importe que ça.
@@ -183,44 +186,44 @@ addGeneralFuelInfo <- function(generators_tbl) {
   generators_fuels_memberships_tbl <- getTableFromPlexos(MEMBERSHIPS_PATH) %>%
     filter(parent_class == "Generator" & child_class == "Fuel") %>%
     rename(generator_name = parent_object,
-           fuel_group = child_object) %>%
+           plexos_fuel_group = child_object) %>%
     # Forcing capital letters on generator names as is convention now
     mutate(generator_name = tolower(generator_name)) %>%
-    select(generator_name, fuel_group)
+    select(generator_name, plexos_fuel_group)
   
   generators_tbl <- generators_tbl %>%
     left_join(generators_fuels_memberships_tbl, by = "generator_name") %>%
-    left_join(fuels_tbl, by = "fuel_group")
+    left_join(fuels_tbl, by = "plexos_fuel_group")
   
   # Adding a column for Antares cluster type
   # NOTE : All power plants outside the use cases defined (e.g. geothermal)
   # are then removed and thus will not be studied.
   # Finer implementation needs to be adjusted here.
   generators_tbl <- generators_tbl %>%
-    mutate(cluster_type = case_when(
-      grepl("Sol$", fuel_group) ~ "Solar PV",
-      grepl("Csp$", fuel_group) ~ "Solar Thermal",
+    mutate(antares_cluster_type = case_when(
+      grepl("Sol$", plexos_fuel_group) ~ "Solar PV",
+      grepl("Csp$", plexos_fuel_group) ~ "Solar Thermal",
       
-      grepl("Win$", fuel_group) ~ "Wind Onshore",
-      grepl("Wof$", fuel_group) ~ "Wind Offshore",
+      grepl("Win$", plexos_fuel_group) ~ "Wind Onshore",
+      grepl("Wof$", plexos_fuel_group) ~ "Wind Offshore",
       
-      grepl("Gas$", fuel_group) ~ "Gas",
-      grepl("Cog$", fuel_group) ~ "Gas", # Note that cogeneration is treated as regular gas
-      grepl("Coa$", fuel_group) ~ "Hard Coal",
-      grepl("Pet$", fuel_group) ~ "Oil",
-      grepl("Nuc$", fuel_group) ~ "Nuclear",
-      grepl("Bio$", fuel_group) ~ "Mixed Fuel", # Note that bio is treated as mixed fuel
-      grepl("Was$", fuel_group) ~ "Mixed Fuel", # Note that waste is treated as mixed fuel
-      grepl("Oil$", fuel_group) ~ "Oil",
+      grepl("Gas$", plexos_fuel_group) ~ "Gas",
+      grepl("Cog$", plexos_fuel_group) ~ "Gas", # Note that cogeneration is treated as regular gas
+      grepl("Coa$", plexos_fuel_group) ~ "Hard Coal",
+      grepl("Pet$", plexos_fuel_group) ~ "Oil",
+      grepl("Nuc$", plexos_fuel_group) ~ "Nuclear",
+      grepl("Bio$", plexos_fuel_group) ~ "Mixed Fuel", # Note that bio is treated as mixed fuel
+      grepl("Was$", plexos_fuel_group) ~ "Mixed Fuel", # Note that waste is treated as mixed fuel
+      grepl("Oil$", plexos_fuel_group) ~ "Oil",
       
-      grepl("Hyd$", fuel_group) ~ "Hydro", # It's debateable whether or not this will be useful.
+      grepl("Hyd$", plexos_fuel_group) ~ "Hydro", # It's debateable whether or not this will be useful.
       # Hydro will probably require a very different implementation than the usual generators_tbl.
-      grepl("Geo$", fuel_group) ~ "Other", # yep, Geothermal is on the menu
+      grepl("Geo$", plexos_fuel_group) ~ "Other", # yep, Geothermal is on the menu
       # Group: 'Other 1' is not a valid name recognized by Antares, you should be using one of: Gas, Hard coal, Lignite, Mixed fuel, Nuclear, Oil, Other, Other 2, Other 3, Other 4
       
-      grepl("Sto$", fuel_group) ~ "Other 2",
-      grepl("Wav$", fuel_group) ~ "Other 3",
-      grepl("Oth$", fuel_group) ~ "Other 4",
+      grepl("Sto$", plexos_fuel_group) ~ "Other 2",
+      grepl("Wav$", plexos_fuel_group) ~ "Other 3",
+      grepl("Oth$", plexos_fuel_group) ~ "Other 4",
       # Dans les graphes, il y a une catégorie "Other" et je pense qu'on va pas se faire chier à importer par la suite
       # chaque truc individuellement alors qu'on peut juste mettre un "Other" tou poti.
       # cependant, c'est bête de perdre de l'information quand on pourrait un jour s'y intéresser.
@@ -229,11 +232,11 @@ addGeneralFuelInfo <- function(generators_tbl) {
       
       TRUE ~ NA_character_  # For unrelated child_object values (other)
     )) %>%
-    filter(!is.na(cluster_type)) %>%
+    filter(!is.na(antares_cluster_type)) %>%
     
     # Chosen conventional order :
     # from most global (e.g. continent) to more informative (e.g. node)
-    select(generator_name, continent, node, fuel_group, cluster_type, fuel_type)
+    select(generator_name, continent, node, plexos_fuel_group, plexos_fuel_type, antares_cluster_type)
   
   return(generators_tbl)
 }
@@ -241,8 +244,70 @@ addGeneralFuelInfo <- function(generators_tbl) {
 # example <- addGeneralFuelInfo(example)
 # print(example)
 
+# Function to get just the year
+# Should be in utils tbh
+extract_date_from_excel_integer <- function(excel_int) {
+  format(as.Date(excel_int, origin = "1899-12-30"), "%Y-%m-%d")
+}
+# # Excel stores dates as the number of days since 1900-01-01 (or 1899-12-30 if you're taking Excel's leap year bug into account
+# extract_year_from_excel_integer <- function(excel_int) {
+#   as.numeric(format(as.Date(excel_int, origin = "1899-12-30"), "%Y"))
+# }
 
+extract_year_from_date <- function(ymd) {
+  as.numeric(format(as.Date(ymd, format = "%Y-%m-%d"), "%Y"))
+}
 
+getBaseGeneratorData <- function(generators_tbl) {
+  properties_tbl <- getTableFromPlexos(PROPERTIES_PATH) %>%
+    filter(collection == "Generators") %>%
+    filter(property %in% c("Max Capacity", "Units", "Commission Date")) %>%
+    select(child_object, property, value)
+  
+  inactive_in_2015 <- getTableFromPlexos(PROPERTIES_PATH) %>%
+    filter(memo == "With the exception of JPN_Nuc_Sendai13837 no active nuclear generators in Japan by 2015 due to Fukushima accident. https://en.wikipedia.org/wiki/Nuclear_power_in_Japan") %>%
+    pull(child_object) %>%
+    unique() %>%
+    tolower()
+  
+  # print(inactive_in_2015)
+  
+  properties_tbl <- properties_tbl %>%
+    filter(!(property == "Units" & value == 0)) %>%
+    # No information is lost by removing these rows : either the generators are never active,
+    # or they are, but the line is a duplicate to indicate different scenarios.
+    pivot_wider(names_from = property, values_from = value) %>%
+    mutate(generator_name = tolower(child_object),
+           nb_units = Units,
+           nominal_capacity = `Max Capacity`,
+           commission_date = extract_date_from_excel_integer(`Commission Date`),
+           active_in_2015 = !(extract_year_from_date(commission_date) > 2015 | generator_name %in% inactive_in_2015)
+           ) %>%
+    select(generator_name, nb_units, nominal_capacity, commission_date, active_in_2015)
+  
+  generators_tbl <- generators_tbl %>%
+    left_join(properties_tbl, by = "generator_name") %>%
+    select(generator_name, continent, node, nominal_capacity, nb_units, commission_date, active_in_2015, antares_cluster_type, plexos_fuel_type, plexos_fuel_group)
+  
+  # generators_tbl <- generators_tbl %>%
+  #   filter(!generator_name %in% generator_names_to_remove)
+  
+  # %>% # Maybe scenario ? eh
+    # filter(`Units` != 0) %>% # faudrait filtrer avec scénarios en sah
+    # pivot_wider(names_from = property, values_from = value)
+    # #mutate(generator_name = tolower(child_object))
+  
+  # properties_tbl <- properties_tbl %>%
+  #   mutate()
+
+# 
+#   print(properties_tbl$scenario %>% unique())
+#   print(properties_tbl$memo %>% unique())
+#   print(properties_tbl, n = 100)
+# 
+
+  return(generators_tbl)
+}
 
 
 
